5fbea0cfa7246f36d3739758cf477657b5bd215b
Gabriel Weidmann <gabriel@weidmann.net>
2026-08-29 08:50:27 +0200

    Enable pull-to-refresh at app root

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+import PullToRefresh from './PullToRefresh'
 import './styles.css'
 import './auth.css'
 import './gallery.css'
@@ -10,6 +11,8 @@ registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PullToRefresh>
+      <App />
+    </PullToRefresh>
   </StrictMode>,
 )
