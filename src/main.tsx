4b74f0ff617c7a6c8dbb0fcfbe975545c769b255
Gabriel Weidmann <gabriel@weidmann.net>
2026-08-29 07:38:23 +0200

    Load header auth styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './styles.css'
+import './auth.css'
 
 registerSW({ immediate: true })
 
